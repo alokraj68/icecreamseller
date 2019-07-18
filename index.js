8e4bea2e773137db98ae11da67b9a9412833f9a2
Alok Rajasukumaran <alokraj68@gmail.com>
2019-07-18 15:02:55 +0530

    Update index.js
    made port dynamic from process.env

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.port || process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen('3000');
-console.log('Express File server in 3000. Access at /public');
+app.listen(port);
+console.log('Express File server is up in port ',port);
